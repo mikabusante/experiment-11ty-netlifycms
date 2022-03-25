17be6f4cd5c978a4c46f4314aa76d0c117a8d0aa
Mika Busante <mika@mikabusante.com>
2022-03-25 16:08:44 -0400

    rm require

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-const eleventyVue = require("@11ty/eleventy-plugin-vue");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/admin");
