3af04a2de276bc438c77b9683df0900447206fca
Mika Busante <mika@mikabusante.com>
2022-03-25 17:21:17 -0400

    Revert "trying parcel + vue"
    This reverts commit 68f465871fc3c67a1edf4b7aed6c9c0355b30dd5.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,11 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.setBrowserSyncConfig({
-    files: ["./dist/**/*.css", "./dist/**/*.js"],
-  });
 
   return {
     dir: {
       input: "src",
-      output: "dist",
+      output: "public",
       includes: "_includes",
       layouts: "_layouts",
     },
