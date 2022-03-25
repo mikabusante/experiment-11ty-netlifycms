73fe13934560834238dd7e9729349c09f1f3f7e5
Mika Busante <mika@mikabusante.com>
2022-03-25 14:52:36 -0400

    cleaning up config

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,8 @@
 const eleventyVue = require("@11ty/eleventy-plugin-vue");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyVue);
-
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.addPassthroughCopy("./src/components");
-
-  eleventyConfig.addWatchTarget("./src/components/");
-  eleventyConfig.addWatchTarget("./src/_includes/");
-
-  eleventyConfig.addTransform("add-html-doctype", (content, outputPath) => {
-    let doctype = "<!doctype html>";
-    // If we’re writing to an HTML file and a Doctype does not already exist
-    if (
-      outputPath.endsWith(".html") &&
-      !content.trim().toLowerCase().startsWith(doctype)
-    ) {
-      return `${doctype}${content}`;
-    }
-    return content;
-  });
 
   return {
     dir: {
